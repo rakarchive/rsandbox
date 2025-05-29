565c14eb4ad60c49540b7e91b59af5fbc2c10c73
Rak Laptudirm <rak@laptudirm.com>
2025-05-29 16:56:03 +0530

    chore: add inspect function

--- cmd/RSAndbox/main.go
+++ cmd/RSAndbox/main.go
@@ -11,17 +11,12 @@ import (
 )
 
 type context struct {
-	people []entity
-}
-
-type entity struct {
-	name string
-	key  *rsa.PrivateKey
+	people map[string]*rsa.PrivateKey
 }
 
 func main() {
 	context := context{
-		people: make([]entity, 0),
+		people: make(map[string]*rsa.PrivateKey),
 	}
 
 	// Prerun some commands
@@ -61,8 +56,10 @@ func (context *context) RunCmd(cmd, args string, auto bool) error {
   list           List the available people and their keys.
   add            Add a new person and generate a new key pair for them.`)
 	case "list":
-		for i, entity := range context.people {
-			fmt.Printf("  %2d. %-10s (\x1b[34m%s\x1b[0m)\n", i+1, entity.name, entity.key)
+		i := 1
+		for name, key := range context.people {
+			fmt.Printf("  %2d. %-10s (\x1b[34m%s\x1b[0m)\n", i, name, key)
+			i += 1
 		}
 	case "add":
 		for _, name := range strings.Split(args, " ") {
@@ -71,11 +68,26 @@ func (context *context) RunCmd(cmd, args string, auto bool) error {
 				fmt.Printf("  sandbox: %s\n", err)
 				break
 			}
-			context.people = append(context.people, entity{name, key})
+			context.people[name] = key
 			fmt.Printf("  \x1b[32msandbox\x1b[0m: added person \x1b[33m%s\x1b[0m with key \x1b[34m%s\x1b[0m\n", name, key)
 		}
 	case "quit", "exit":
 		return errorStop
+	case "inspect":
+		key, found := context.people[args]
+		if !found {
+			return fmt.Errorf("no person named \x1b[31m%s\x1b[0m found", args)
+		}
+
+		fmt.Println("  \x1b[32mPublic data:\x1b[0m")
+		fmt.Printf("    \x1b[32mN\x1b[0m    = %X\n", key.N)
+		fmt.Printf("    \x1b[32mE\x1b[0m    = %X\n", key.E)
+
+		fmt.Println("  \x1b[31mPrivate data:\x1b[0m")
+		fmt.Printf("    \x1b[31mP\x1b[0m    = %X\n", key.P)
+		fmt.Printf("    \x1b[31mQ\x1b[0m    = %X\n", key.Q)
+		fmt.Printf("    \x1b[31mλ(N)\x1b[0m = %X\n", key.LambdaN())
+		fmt.Printf("    \x1b[31mD\x1b[0m    = %X\n", key.D)
 	default:
 		fmt.Print("\x1b[1F\x1b[0K")
 		fmt.Printf("\x1b[31m%s %s\x1b[0m\n", cmd, args)
